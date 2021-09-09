9ef19a0a0c17cb9dd38c4348e8586ef64a4ab98b
Pavel Napreenko <pavel1119@mail.ru>
2021-09-09 16:10:48 +0300

    stage 1 old editions

--- src/main/java/ru/pnapreenko/blogengine/api/response/AuthResponse.java
+++
@@ -1,8 +0,0 @@
-package ru.pnapreenko.blogengine.api.response;
-
-import lombok.Data;
-
-@Data
-public class AuthResponse {
-
-}
--- src/main/java/ru/pnapreenko/blogengine/model/dao/SettingsDAO.java
+++
@@ -1,8 +0,0 @@
-package ru.pnapreenko.blogengine.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.pnapreenko.blogengine.model.GlobalSettings;
-
-@Repository
-public interface SettingsDAO extends CrudRepository<GlobalSettings, Long> {}
