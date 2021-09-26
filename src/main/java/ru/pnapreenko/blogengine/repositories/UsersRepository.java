3660a16e574d07e4b3b395ae19610ac8486102bc
Pavel Napreenko <pavel1119@mail.ru>
2021-09-26 13:27:42 +0300

    Users repository.

--- src/main/java/ru/pnapreenko/blogengine/repositories/UsersRepository.java
+++ src/main/java/ru/pnapreenko/blogengine/repositories/UsersRepository.java
@@ -4,5 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.pnapreenko.blogengine.model.User;
 
+import java.util.List;
+
 @Repository
-public interface UsersRepository extends CrudRepository<User, Integer> {}
+public interface UsersRepository extends CrudRepository<User, Integer> {
+    List<User> findByIsModeratorTrue();
+    List<User> findByIsModeratorFalse();
+    User findByEmail(String email);
+    User findByCode(String code);
+}
