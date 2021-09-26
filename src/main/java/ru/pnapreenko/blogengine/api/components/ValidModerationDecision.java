b8a3d72ac56efa746880c39e0bc199f2bd97311d
Pavel Napreenko <pavel1119@mail.ru>
2021-09-26 14:00:22 +0300

    valid mod-on decision

---
+++ src/main/java/ru/pnapreenko/blogengine/api/components/ValidModerationDecision.java
@@ -0,0 +1,34 @@
+package ru.pnapreenko.blogengine.api.components;
+
+import ru.pnapreenko.blogengine.enums.ModerationDecision;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidModerationDecision.ModerationDecisionValidator.class)
+@Documented
+public @interface ValidModerationDecision {
+
+    String message() default "Неверное значение параметра! Используйте 'accept' или 'decline'.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    class ModerationDecisionValidator implements ConstraintValidator<ValidModerationDecision, String> {
+        @Override
+        public boolean isValid(String decision, ConstraintValidatorContext context) {
+            try {
+                ModerationDecision.valueOf(decision.toUpperCase());
+                return true;
+            } catch (IllegalArgumentException ignored) {
+                return false;
+            }
+        }
+    }
+}
