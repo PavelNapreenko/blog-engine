2b14490ce09987a1a4a974faf90135773d724ea6
Pavel Napreenko <pavel1119@mail.ru>
2021-09-26 14:02:10 +0300

    post date converter

---
+++ src/main/java/ru/pnapreenko/blogengine/api/components/PostDateConverter.java
@@ -0,0 +1,46 @@
+package ru.pnapreenko.blogengine.api.components;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import ru.pnapreenko.blogengine.api.utils.DateUtils;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+
+@JsonComponent
+public class PostDateConverter {
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm";
+    private static final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+
+    public static class Serialize extends JsonSerializer<Instant> {
+        @Override
+        public void serialize(Instant value, JsonGenerator jsonGenerator, SerializerProvider provider) {
+            try {
+                if (value == null) {
+                    jsonGenerator.writeNull();
+                } else {
+                    jsonGenerator.writeString(DateUtils.formatDate(value, DATE_FORMAT));
+                }
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+    public static class Deserialize extends JsonDeserializer<Instant> {
+        @Override
+        public Instant deserialize(JsonParser jsonparser, DeserializationContext context) {
+            try {
+                String date = jsonparser.getText();
+                return (date != null) ? Instant.ofEpochMilli(sdf.parse(date).getTime()) : null;
+            } catch (Exception ignored) {
+            }
+            return null;
+        }
+    }
+}
+
