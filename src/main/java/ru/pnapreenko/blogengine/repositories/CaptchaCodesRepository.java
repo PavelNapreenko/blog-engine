f14f1f958d017699a09a1d2d36c47c55b03762f7
Pavel Napreenko <pavel1119@mail.ru>
2021-09-26 13:38:56 +0300

    Captcha codes repository

--- src/main/java/ru/pnapreenko/blogengine/repositories/CaptchaCodesRepository.java
+++ src/main/java/ru/pnapreenko/blogengine/repositories/CaptchaCodesRepository.java
@@ -1,8 +1,19 @@
 package ru.pnapreenko.blogengine.repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.pnapreenko.blogengine.model.CaptchaCode;
 
+import java.time.Instant;
+
 @Repository
-public interface CaptchaCodesRepository extends CrudRepository<CaptchaCode, Integer> {}
+@Transactional
+public interface CaptchaCodesRepository extends CrudRepository<CaptchaCode, Integer> {
+
+    @Modifying
+    void deleteByTimeBefore(Instant time);
+
+    CaptchaCode findBySecretCode(String secretCode);
+}
